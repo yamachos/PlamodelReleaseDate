75090028e024a0caaad8509e86a37948290a6ff3
shiroya manaka <shiroya2018@outlook.jp>
2026-08-06 22:33:22 +0900

    カレンダー設定を追加

---
+++ PlamodelReleaseDate/resource/settings.py
@@ -0,0 +1,18 @@
+calendars = [
+    {   # ガンプラ
+        'url' : 'aa4ffc82cae7b1a3c87273a10e1cebaf07d3f1d1e5eb3004e8448f7b95e7d04c@group.calendar.google.com',
+        'filter' : ['MGSD', 'HG', 'MG', 'RG', 'PG', 'SD', 'EG', 'RE/100', 'FM']
+    },
+    {   # 30MS/MP
+        'url' : 'a26172608d3612eeeb7d862b1323ab7d10d2291bb3a586234a1ce8110e0b81eb@group.calendar.google.com',
+        'filter' : ['30MS', '30MP']
+    },
+    {   #30MM/MF
+        'url' : 'dbc30c769fa3bf4fa73c7eb951c8e12f4a2185374f9a25198d16b36e2971a290@group.calendar.google.com',
+        'filter' : ['30MM', '30MF']
+    },
+    {   # その他
+        'url' : '5ddaedf490073d747edc8b64da13e4576f4aad0b95b747f4ccf607e1f2eaca72@group.calendar.google.com',
+        'filter' : ['FrS Amplified', 'FrS', 'etc.']
+    }
+]
